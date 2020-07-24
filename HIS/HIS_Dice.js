42ec2d91202fa2ad1822845f86ae90829951b46c
ia-chow <csop0329@gmail.com>
2020-07-24 16:05:11 -0400

    js update

--- HIS/HIS_Dice.js
+++ HIS/HIS_Dice.js
@@ -2,5 +2,5 @@ function get_debater_dice(value, team, language, name, status, tmore, inq, augsb
     num_dice = value
 }
 function get_reform_odds(atk_dice, def_dice, win_ties, bible_trans){
-
+    var deb_data = JSON.parse(debater_values.json)
 }
