ea6e04cebf5ef4f0432baea18f6b6cf8296af43b
ia-chow <csop0329@gmail.com>
2020-07-27 11:46:16 -0400

    finished converting the get_reform_odds into js

--- HIS/HIS_Dice.js
+++ HIS/HIS_Dice.js
@@ -1,3 +1,6 @@
+// CONSTANTS:
+const DICE_FACES = 6 //number of faces on a die
+
 function get_debater_dice(value, team, language, name, status, tmore, inq, augsburg){
     /*
     Gets the number of dice each debater rolls in a debate
@@ -9,14 +12,34 @@ function get_debater_dice(value, team, language, name, status, tmore, inq, augsb
     num_dice = value
 
 }
-function get_reform_odds(atk_dice, def_dice, win_ties, bible_trans){
+function get_reform_odds(atk_dice, def_dice, win_ties, bible_trans, dice_faces=DICE_FACES){
     /*
-    TODO: FINISH DOCSTRING FOR THIS FUNCTION AND SEE IF IT WORKS
-    ALSO FINISH REWRITING THIS FUNCTION FROM PYTHON TO JS AS WELL
+    param int atk_dice: number of dice the attacker rolls
+    param int def_dice: number of dice the defender rolls 
+    param bool win_ties: whether attacker wins ties or not (T/F)
+    param dice_faces: default param, number of dice faces
     */
     var atk_win = 0
     var def_win = 0
+    var val;
+    for (val = 1; val < dice_faces + 1; val++){
+        const prob_def = ((val/dice_faces) ** def_dice) - (((val - 1)/dice_faces) ** def_dice); // probability that highest defender roll is equal to this value
+        
+        const prob_atk_less = ((val - 1)/dice_faces) ** atk_dice;  // probability that all defender dice are lower than this value
+        const prob_equal = (val/dice_faces) ** atk_dice - prob_atk_less; // probability that highest attacker roll is exactly equal to the given roll (tie)
+        const prob_atk_more = 1 - prob_atk_less - prob_equal; // probability that highest attacker roll is greater than given value
+
+        atk_win += prob_def * prob_atk_more;
+        def_win += prob_def * prob_atk_less;
+        if (win_ties == true){
+            atk_win += prob_equal * prob_def;
+        }
+        else if (win_ties == false){
+            def_win += prob_equal * prob_def;
+        }
+        //console.log(atk_win, def_win, prob_def, prob_atk_less, prob_atk_more, prob_equal);
+    }
+    return [atk_win, def_win];
 }
 
-console.log("Hello, World!");
-console.log(get_reform_odds(3, 3, 3, 3));
+console.log(get_reform_odds(3, 4, true, 3));
